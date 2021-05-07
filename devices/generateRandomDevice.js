0131ea7397721eef1a85f210ac9fd5a950456226
Igor Ponomarev <misterzicold@gmail.com>
2021-05-08 01:26:33 +0700

    add generate cypher

--- devices/generateRandomDevice.js
+++ devices/generateRandomDevice.js
@@ -3,12 +3,20 @@ const pathToCyphers = "Cyphers_Official_Books.xml";
 const pathToArtefacts = "Artefacts_Official_Books.xml";
 
 let odditiesXml;
+let cyphersXml;
+let artefactsXml;
 
 $(document).ready(function () {
     loadDevicesXmlSync(pathToOddities, loadOdditiesXml);
+    loadDevicesXmlSync(pathToCyphers, loadCyphersXml);
+    loadDevicesXmlSync(pathToArtefacts, loadArtefactsXml);
 
     $("#generate_oddity").click(function () {
         generateRandomOddity();
+    });
+
+    $("#generate_cypher").click(function () {
+        generateRandomCypher();
     });
 
 });
@@ -26,20 +34,46 @@ function loadDevicesXmlSync(pathToXML, loadingFunction) {
     });
 }
 
+
+
 function loadOdditiesXml(xmlData) {
     odditiesXml = xmlData;
 }
 
+function loadCyphersXml(xmlData) {
+    cyphersXml = xmlData;
+}
+
+function loadArtefactsXml(xmlData) {
+    artefactsXml = xmlData;
+}
+
+
+
+function generateRandomCypher() {
+    let randomDevice = getRandomDevice(cyphersXml, 'Cypher');
+
+    let html = "";
+    html += encloseDeviceProperty(randomDevice, 'Name');
+    html += encloseDeviceProperty(randomDevice, 'Level');
+    html += encloseDeviceProperty(randomDevice, 'Effect');
+
+    html += encloseDeviceProperty(randomDevice, 'Usable');
+    html += encloseDeviceProperty(randomDevice, 'Wearable');
+    html += encloseDeviceProperty(randomDevice, 'Internal');
+
+    html += encloseDeviceProperty(randomDevice, 'RollTable');
+    html += encloseDeviceProperty(randomDevice, 'Source');
+    $("#cypher_description").html(html);
+}
+
 function generateRandomOddity() {
-    var randomDevice = getRandomDevice(odditiesXml, 'Oddity');
+    let randomDevice = getRandomDevice(odditiesXml, 'Oddity');
 
-    var html = "";
-    var description = randomDevice.find('Description').html();
-    var source = randomDevice.find('Source').text();
-    html += "<label>Description: " + description + "</label><br>";
-    html += "<label>Source: " + source + "<label><br>";
+    let html = "";
+    html += encloseDeviceProperty(randomDevice, 'Description');
+    html += encloseDeviceProperty(randomDevice, 'Source');
     $("#oddity_description").html(html);
-
 }
 
 function getRandomDevice(xmlData, deviceTypeName) {
@@ -49,6 +83,13 @@ function getRandomDevice(xmlData, deviceTypeName) {
     var randomDevice = $(xmlData).find(deviceTypeName).eq(index);
     return randomDevice;
 }
+
+function encloseDeviceProperty(deviceXml, propertyName) {
+    let text = deviceXml.find(propertyName).text();
+    let result = `<div><b>${propertyName}:</b> ${text}</div>`;
+    return result;
+}
+
 
 // Get a number from 0 to max-1
 function getRandomInt(max) {
